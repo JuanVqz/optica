897cb03733f1272219bdb271e099f7ed5d174449
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-25 22:55:44 -0600

    se agregó el método total_en_letras en helper ventas

--- app/helpers/ventas_helper.rb
+++ app/helpers/ventas_helper.rb
@@ -1,5 +1,9 @@
 module VentasHelper
 
+  def total_en_letras venta
+    "#{venta.total.to_i.to_words} pesos con #{venta.centavos.to_i.to_words} centavos"
+  end
+
   def estado_de_pago venta
     content_tag :span, class: "badge #{badge_saldada(venta)}" do
       venta.saldada? ? 'PAGADO' : 'PENDIENTE'
