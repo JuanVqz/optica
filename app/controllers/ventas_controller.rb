3f0c6efe780654b674970ed25f211238eef6bba7
Juan Vásquez <juanthoniovg@gmail.com>
2019-03-30 15:34:51 -0600

    se agregó la notificación al crear una venta nueva

--- app/controllers/ventas_controller.rb
+++ app/controllers/ventas_controller.rb
@@ -20,7 +20,7 @@ class VentasController < ApplicationController
 
     respond_to do |format|
       if @venta.save
-        VentasMailer.nueva(@venta).deliver
+        NotificarVentaNuevaWorker.perform_async(@venta.id)
         format.html { redirect_to @venta, notice: 'La venta fue creada correctamente' }
         format.json { render :show, status: :created, location: @venta }
       else
